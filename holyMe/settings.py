43221970177f9822e45fa3e6c2050d58bee66c8a
palms1 <palmor14@gmail.com>
2018-03-18 20:00:21 +0000

    requirements problems5

--- holyMe/settings.py
+++ holyMe/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['holyme-prod.herokuapp.com']
+ALLOWED_HOSTS = ['holyme-prod.herokuapp.com, holyme.com.br, www.holyme.com.br']
 
 
 # Application definition
